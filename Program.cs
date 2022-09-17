eff41b5ccf5862a6ce87c7966de2df5312b4f00d
«Anatoly <haimka71@mail.ru>
2022-09-17 14:07:12 +0300

    Seminar Task 43

--- Program.cs
+++ Program.cs
@@ -132,6 +132,8 @@
 //   Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами равна {result}");
 
 
+
+
 	
 
 
@@ -140,34 +142,93 @@
 	// -1, -7, 567, 89, 223-> 3
 	
 
-	Console.Clear();
+	// Console.Clear();
 		
-	Console.Write($"Введите число М (количество чисел): ");
-	int m = Convert.ToInt32(Console.ReadLine());           //Ввод пользователем числа элементов массива M
-	int[] massNumb = new int[m];
+	// Console.Write($"Введите число М (количество чисел): ");
+	// int m = Convert.ToInt32(Console.ReadLine());           //Ввод пользователем числа элементов массива M
+	// int[] massNumb = new int[m];
 	
-	void InputNumbers(int m)                               //Метод ввода пользователем M чисел поочередно
+	// void InputNumbers(int m)                               //Метод ввода пользователем M чисел поочередно
+  // {
+	// for (int i = 0; i < m; i++)
+	//   {
+	//     Console.Write($"Введите {i+1} число: ");
+	//     massNumb[i] = Convert.ToInt32(Console.ReadLine());  
+	//   }
+	// }
+	
+
+	// int NumbLessZero(int[] massNumb)                     //метод сортировки элементов массива по условию, возврат счетчика полож-х чисел
+	// {
+	//   int count = 0;
+	//   for (int i = 0; i < massNumb.Length; i++)
+	//   {
+	//     if(massNumb[i] > 0 ) count += 1;             
+	//   }
+	//   return count;
+	// }
+	
+	// InputNumbers(m);
+	// Console.WriteLine();
+	// Console.WriteLine($"Введено чисел больше 0: {NumbLessZero(massNumb)} ");
+	
+
+
+
+
+
+
+
+
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+Console.Clear();
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+
+void InputCoeff()                               // метод: считываем с консоли коэффициенты уравнения прямой
+{
+  for (int i = 0; i < coeff.GetLength(0); i++)
   {
-	for (int i = 0; i < m; i++)
-	  {
-	    Console.Write($"Введите {i+1} число: ");
-	    massNumb[i] = Convert.ToInt32(Console.ReadLine());  
-	  }
-	}
-	
+    Console.Write($"Введите коэффициенты {i+1}-й прямой (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
 
-	int NumbLessZero(int[] massNumb)                     //метод сортировки элементов массива по условию, возврат счетчика полож-х чисел
-	{
-	  int count = 0;
-	  for (int i = 0; i < massNumb.Length; i++)
-	  {
-	    if(massNumb[i] > 0 ) count += 1;             
-	  }
-	  return count;
-	}
-	
-	InputNumbers(m);
-	Console.WriteLine();
-	Console.WriteLine($"Введено чисел больше 0: {NumbLessZero(massNumb)} ");
-	
 
+double[] CrossResult(double[,] coeff)               // метод: рассчитываем возможные точки пересечения прямых
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+
+void CrossLines(double[,] coeff)                // метод: рассчитываем параллельность либо совпадение прямых по коэфф-м наклона и смещения
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    CrossResult(coeff);
+    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InputCoeff();
+CrossLines(coeff);
